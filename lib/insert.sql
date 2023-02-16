ecf28b1280f709d0e35dd587b363a60f7314e195
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-02-16 20:23:35 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears  (id, name, age, sex, color,temperament,alive) VALUES 
+(1, "Mr. Chocolate", 2, "F", "Black", "shy", 1 ),
+(2, "Rowdy", 3, "M", "Brown", "gentle", 1 ),
+(3, "Tabitha", 4, "M", "White", "Protective", 0),
+(4, "Sergent Brown", 5, "M", "Grey", "Impulsive", 0),
+(5, "Melissa", 6, "F", "White", "Calm", 1),
+(6, "Grinch", 7, "F", "Black", "Easygoing", 1),
+(7, "Wendy", 8, "F", "White", "Carefree", 0),
+(8, null, 9, "M", "Black", "Playful", 1);
